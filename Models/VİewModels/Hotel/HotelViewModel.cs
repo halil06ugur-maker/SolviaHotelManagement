4af34e25438e3a1cefaa77b49ad67c8ae8b61e89
Halil Uğur <halil06ugur@gmail.com>
2025-09-13 18:03:15 +0300

    HotelViewModel Created

---
+++ Models/VİewModels/Hotel/HotelViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SolviaHotelManagement.Models.VİewModels.Hotel
+{
+    public class HotelViewModel
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string taxNumber { get; set; }
+        public string phoneNumber { get; set; }
+        public DateTime createdDate { get; set; }
+
+    }
+}
