5b8f3c732da403d5d686591ce364d18b24d3d761
Halil Uğur <halil06ugur@gmail.com>
2025-09-13 17:05:45 +0300

    Hotel

---
+++ Controllers/Hotel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SolviaHotelManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Hotel : ControllerBase
+    {
+    }
+}
